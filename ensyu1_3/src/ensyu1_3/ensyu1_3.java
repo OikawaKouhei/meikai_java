12e736ebdc81835f34bc13cd172a6440c4b2cb7c
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-05 13:43:21 +0900

    演習1_3 「及川康平」を一文字ずつ改行し、「及川」と「康平」の間を改行して出力されるようなプログラム。 表示例： 及 川
    康
    平

---
+++ ensyu1_3/src/ensyu1_3/ensyu1_3.java
@@ -0,0 +1,26 @@
+package ensyu1_3;
+/*
+ * クラス名：ensyu1_3
+ * 概要：演習問題1_3
+ * 作成者：Oikawa Kouhei
+ * 作成日：2023/04/05
+ */
+public class ensyu1_3 {
+/*
+ * 関数名：mainメソッド
+ * メソッドの説明：及川康平を表示、また、各文字を１文字ずつ改行、「及川」と「康平」の間を改行
+ * 引数：String[] args
+ * 返り値：及川康平
+ * パラメーター：及\n川\n\n康\n平"
+ */
+	public static void main(String[] args) {
+/*
+ * System.out.println("");  ：因数を画面に出力する処理
+ * 及\n川\n\n康\n平  ：「及川康平」を一文字ずつ改行して、「及川」と「康平」の間もさらに改行するための処理
+ */
+		System.out.println("及\n川\n\n康\n平");
+// mainメソッドを閉じる処理
+	}
+// class ensyu1_3を閉じる処理
+
+}
