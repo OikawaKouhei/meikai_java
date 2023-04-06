f3d019cd6814d455429632352a1cd0faf1f3e4f0
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-06 11:30:08 +0900

    訂正点  ・10行目から19行目にかけてのインデントの統一  ・20行目から42行目にかけてのインデントの統一  ・変数の変更：x＝firstNumber , y＝secondNumber

--- ensyu2_1/src/ensyu2_1/ensyu2_1.java
+++ ensyu2_1/src/ensyu2_1/ensyu2_1.java
@@ -6,36 +6,46 @@ package ensyu2_1;
 * 作成日：2023/4/5
 */
 public class ensyu2_1 {
-    /*
-    * 関数名：main関数
-    * 概要：xとyに少数部をもつ実数値を代入するように変更した場合のプログラムとその考察
-    * 引数：string[] args
-    * 返り値：なし
-    * パラメーターの説明：「xの値は" + x + "です。」「yの値は" + y + "です。」「合計は" + (x + y) + "です。」「平均は" + (x + y) / 2 + "です。」
-    */
+	/*
+	 * 関数名：main関数
+	 * 概要：firstNumberとsecondNumberに少数部をもつ実数値を代入するように変更した場合のプログラムとその考察
+	 * 引数：string[] args
+	 * 返り値：なし
+	 * パラメーターの説明：「firstNumberの値は" + firstNumber + "です。」
+	 * 　　　　　　　　　　「secondNumberの値は" + secondNumber + "です。」
+	 * 　　　　　　　　　　「合計は" + (firstNumber + secondNumber) + "です。」
+	 * 　　　　　　　　　　「平均は" + (firstNumber + secondNumber) / 2 + "です。」
+	 */
 	public static void main(String[] args) {
-        // xに実数値を代入するために、xという変数を宣言した処理
-		int x;
-        // yに実数値を代入するために、yという変数を宣言した処理
-		int y;
-        // 変数xに実数値3.6を代入するための処理
-		x = 3,6;
-        // 変数yに実数値6.4を代入するための処理
-		y = 6.4;
-
-        // 「xの値は" + x + "です。」を出力するための処理
-		System.out.println("xの値は" + x + "です。");
-        // 「yの値は" + y + "です。」を出力するための処理
-		System.out.println("yの値は" + y + "です。");
-        // 「合計は" + (x + y) + "です。」を出力するための処理
-		System.out.println("合計は" + (x + y) + "です。");
-		// 「平均は" + (x + y) / 2 + "です。」を出力するための処理
-		System.out.println("平均は" + (x + y) / 2 + "です。");
+		// firstNumberに実数値を代入するために、firstNumberという変数を宣言する
+		int firstNumber;
 		
+		// secondNumberに実数値を代入するために、secondNumberという変数を宣言する
+		int secondNumber;
+		
+		// 変数firstNumberに実数値3.6を代入する
+		firstNumber = 3.3;
+		
+		// 変数secondNumberに実数値6.4を代入する
+		secondNumber = 6.7;
+
+		// 「firstNumberの値は" + firstNumber + "です。」を出力する
+		System.out.println("firstNumberの値は" + firstNumber + "です。");
+		
+		// 「secondNumberの値は" + secondNumber + "です。」を出力する
+		System.out.println("secondNumberの値は" + secondNumber + "です。");
+		
+		// 「合計は" + (firstNumber + secondNumber) + "です。」を出力する
+		System.out.println("合計は" + (firstNumber + secondNumber) + "です。");
+		
+		// 「平均は" + (firstNumber + secondNumber) / 2 + "です。」を出力する
+		System.out.println("平均は" + (firstNumber + secondNumber) / 2 + "です。");
 	}
 }
+
 
 /*
  * ＜考察＞
  * int型は整数の値しか扱えないためエラーが起きる。
 */
+
