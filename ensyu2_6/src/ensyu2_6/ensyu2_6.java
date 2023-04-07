7f4b2dfa368972df5d474239ed6ea31e74c4cbd0
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-07 10:27:22 +0900

    訂正点  ・変数bottom,highから変数triangleBottom,triangleHighに変更  ・コメントの追加（23・26・31・36行目）

--- ensyu2_6/src/ensyu2_6/ensyu2_6.java
+++ ensyu2_6/src/ensyu2_6/ensyu2_6.java
@@ -14,22 +14,26 @@ public class ensyu2_6 {
 	 * 概要：底辺と高さを実数値で入力し、三角形の面積を求める。
 	 * 引数：String[] args
 	 * 返り値：なし
-	 * パラメーターの説明：bottom , high
+	 * パラメーターの説明：triangleBottom , triangleHigh
 	 */
 	public static void main(String[] args) {
 		// 標準入力を取得するため
 		Scanner standardInput = new Scanner(System.in);
 		
+		// これから、三角形の面積を求めることを知らせるため
 		System.out.println("三角形の面積を求めます。");
 		
+		// 三角形の底辺を、実数値で入力を促すため
 		System.out.print("底辺：");
-		//標準入力した実数値を、変数bottomに代入
-		double bottom = standardInput.nextDouble();
+		//標準入力した実数値を、変数triangleBottomに代入
+		double triangleBottom = standardInput.nextDouble();
 		
+		// 三角形の高さを、実数値で入力を促すため
 		System.out.print("高さ：");
-		//標準入力した実数値を、変数highに代入
-		double high = standardInput.nextDouble();
+		// 標準入力した実数値を、変数trianglehighに代入
+		double triangleHigh = standardInput.nextDouble();
 		
-		System.out.println("面積は" + (bottom * high) / 2 + "です。");
+		// 「(triangleBottom * triangleHigh) / 2 」を計算し、三角形の面積を出力するため
+		System.out.println("面積は" + (triangleBottom * triangleHigh) / 2 + "です。");
 	}
 }
