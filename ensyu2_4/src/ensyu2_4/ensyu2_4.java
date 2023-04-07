a01c974d3bd1b22932abb2843361a186fad60050
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-07 09:58:01 +0900

    訂正点  ・コメントの追加（25・30・32行目）

--- ensyu2_4/src/ensyu2_4/ensyu2_4.java
+++ ensyu2_4/src/ensyu2_4/ensyu2_4.java
@@ -15,19 +15,22 @@ public class ensyu2_4 {
 	 * 概要：標準入力した数値に10を加減した値を、出力する
 	 * 引数：String[] args
 	 * 返り値：なし
-	 * パラメーターの説明：「10を加えた値は" + (result + 10) + "です。"」
-	 * 　　　　　　　　　：「10を減じた値は" + (aresult - 10) + "です。"」
+	 * パラメーターの説明：「10を加えた値は" + (resultNumber + 10) + "です。"」
+	 * 　　　　　　　　　：「10を減じた値は" + (aresultNumber - 10) + "です。"」
 	 */
 	public static void main(String[] args) {
 		// 標準入力を取得するため
 		Scanner standardInput = new Scanner(System.in);
 		
+		// 「整数値：」を画面に出力し、整数値の入力を促すため
 		System.out.print("整数値：");
-		// 標準入力した数値を、resultに代入する
-		int result = standardInput.nextInt();
+		// 標準入力した数値を、resultNumberに代入する
+		int resultNumber = standardInput.nextInt();
 		
-		System.out.println("10を加えた値は" + (result + 10) + "です。");
-		System.out.println("10を減じた値は" + (result - 10) + "です。");
+		// 入力したresultNumberに10を加えた数値を出力するため
+		System.out.println("10を加えた値は" + (resultNumber + 10) + "です。");
+		// 入力したresultNumberに10を減じた数値を出力するため
+		System.out.println("10を減じた値は" + (resultNumber - 10) + "です。");
 	}
 
 }
