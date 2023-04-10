8408cbe07dd84c247089bcd5756d4a8db5d9e1fe
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-10 17:05:38 +0900

    訂正点  ・インデントの編集  ・「エラー」の追加

--- ensyu3_8/src/ensyu3_8/ensyu3_8.java
+++ ensyu3_8/src/ensyu3_8/ensyu3_8.java
@@ -28,20 +28,25 @@ public class ensyu3_8 {
 		
 		// 変数testPointsが、「0～59」かを判定する
 		if (testPoints >= 0 && testPoints <= 59)
-		// 「0～59」だった場合、「不可」と表示する
-		System.out.println("不可");
+			// 「0～59」だった場合、「不可」と表示する
+			System.out.println("不可");
 		// 「0～59」でない場合、変数testPointsが、「60～69」かを判定する
 		else if (testPoints >= 60 && testPoints <= 69)
-		// 「60～69」だった場合、「可」と表示する
-		System.out.println("可");
+			// 「60～69」だった場合、「可」と表示する
+			System.out.println("可");
 		// 「0～59」「60～69」でない場合、変数testPointsが「70～79」かを判定する
 		else if (testPoints >= 70 && testPoints <= 79)
-		// 「70～79」だった場合、「良」と表示する
-		System.out.println("良");
+			// 「70～79」だった場合、「良」と表示する
+			System.out.println("良");
 		// 「0～59」「60～69」「70～79」でない場合、変数testPointsが「80～100」かを判定する
 		else if (testPoints >= 80 && testPoints <= 100)
-		// 「80～100」だった場合、「優」と表示する
-		System.out.println("優");
+			// 「80～100」だった場合、「優」と表示する
+			System.out.println("優");
+		// 「0未満の数値または101以上の数値」を入力したときを判定する
+		else
+			// 「0未満の数値または101以上の数値」だった場合、「エラー」を表示
+			System.out.println("エラー");
+		
 	}
 
 }
