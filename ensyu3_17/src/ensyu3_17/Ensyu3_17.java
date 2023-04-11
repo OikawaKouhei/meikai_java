6c674f1731651e9dd54581ef36667fd0bcbc36f8
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-11 21:07:46 +0900

    演習3_17  ・0,1,2いずれかの値の乱数を生成し、0であれば「グー」、1であれば「チョキ」、2であれば「」パー」を表示するプログラム

---
+++ ensyu3_17/src/ensyu3_17/Ensyu3_17.java
@@ -0,0 +1,35 @@
+package ensyu3_17;
+//	java.util.Randomのパッケージを利用するための宣言
+import java.util.Random;
+/*
+ * クラス名：Ensyu3_17
+ * 概要：演習3_17
+ * 作成者：OikawaKouhei
+ * 作成日：2023/04/11
+ */
+public class Ensyu3_17 {
+	/*
+	 * 関数名：main
+	 * 概要：生成される乱数に応じて、グー、チョキ、パーを生成するプログラム
+	 * 引数：String[] args
+	 * 返り値：なし
+	 * パラメーターの説明：なし
+	 */
+	public static void main(String[] args) {
+		//	乱数を生成するため
+		Random randomNumber = new Random();
+		//	変数luckyNumberを、0から1で初期化する
+		int luckyNumber = randomNumber.nextInt(3);
+		
+		//	0,1,2の生成された乱数に応じて、グー、チョキ、パーを表示するため、
+		switch (luckyNumber) {
+			//	0が生成された場合、「グー」と表示する
+			case 0 : System.out.print("グー");	break;
+			//	1が生成された場合、「チョキ」と表示する
+			case 1 : System.out.print("チョキ");	break;
+			//	2が生成された場合、「パー」と表示する
+			case 2 : System.out.print("パー");	break;
+		}
+	}
+
+}
