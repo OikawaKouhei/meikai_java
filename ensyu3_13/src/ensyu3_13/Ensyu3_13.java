b5ad5f88b48e32532a41d925bfc29084dda26187
OikawaKouhei <ddt.kh.oikawa@gmail.com>
2023-04-12 13:57:13 +0900

    訂正点  ・コードの編集(整数A=2,B=1,C=3と入力した際、中央値が3と表示される原因を編集)

--- ensyu3_13/src/ensyu3_13/Ensyu3_13.java
+++ ensyu3_13/src/ensyu3_13/Ensyu3_13.java
@@ -14,25 +14,31 @@ public class Ensyu3_13 {
 	 * 概要：三つの整数値を読み込みその中央値を求めて表示するプログラム
 	 * 引数：Strin[] args
 	 * 返り値：なし
-	 * パラメーターの説明：なし
+	 * パラメーターの説明：な4し
 	 */
 	public static void main(String[] args) {
 		// 標準入力するため
 		Scanner standardInput = new Scanner(System.in);
 		
-		//	「整数値A：」を表示して、以降に整数値の入力を促す。入力された値を初期化するために、変数firstNumberを生成
-		System.out.print("整数A：");	int firstNumber = standardInput.nextInt();
-		//	「整数値B：」を表示して、以降に整数値の入力を促す。入力された値を初期化するために、変数secondNumberを生成
-		System.out.print("整数B：");	int secondNumber = standardInput.nextInt();
-		//	「整数値C：」を表示して、以降に整数値の入力を促す。入力された値を初期化するために、変数thirdNumberを生成
-		System.out.print("整数C：");	int thirdNumber = standardInput.nextInt();
+		//「整数値A：」を表示して、以降に整数値の入力を促す
+		System.out.print("整数A：");
+		// 変数firstNumberを生成し、入力された値で初期化する
+		int firstNumber = standardInput.nextInt();
+		//「整数値B：」を表示して、以降に整数値の入力を促す
+		System.out.print("整数B：");
+		// 変数secondNumberを生成し、入力された値で初期化する
+		int secondNumber = standardInput.nextInt();
+		//	「整数値C：」を表示して、以降に整数値の入力を促す
+		System.out.print("整数C：");
+		// 変数thirdNumberを生成し、入力された値で初期化する
+		int thirdNumber = standardInput.nextInt();
 		
 		//	のちに、中央値を代入するために、変数medianNumberを生成し、firstNumberで初期化する
 		int medianNumber = firstNumber;
 		//	secondNumberがmedianNumberより大きいと判定したら、medianNumberにsecondNumberを代入する
-		if (secondNumber > medianNumber) { medianNumber = secondNumber; }
-		//	thirdNumberがmedianNumberより小さいと判定したら、medianNumberにthirdNumberを代入する
-		if (thirdNumber < medianNumber) { medianNumber = thirdNumber; }
+		if (medianNumber < secondNumber) { medianNumber = secondNumber; }
+		//	thirdNumberがmedianNumberより大きいと判定したら、medianNumberにthirdNumberを代入する
+		if (medianNumber > thirdNumber) { medianNumber = thirdNumber; }
 		
 		//	最終的に代入されたmedianNumberを中央値として表示する。
 		System.out.println("中央値は" + medianNumber + "です。");
